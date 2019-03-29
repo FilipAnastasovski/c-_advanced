c2ee01cb0f72e1c3fbeeafb557a8b040a1b692d9
FilipAnastasovski <45153380+FilipAnastasovski@users.noreply.github.com>
2019-03-29 15:47:49 +0100

    Add files via upload

---
+++ Advanced_cSharp_h1/Advanced_cSharp_h1/Cat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_cSharp_h1
+{
+    public class Cat : IPredator
+    {
+        public int AttackSpeed { get; set; }
+        public string Name { get; set; }
+
+        public void Attack(IPray pray)
+        {
+            if (AttackSpeed > pray.FleeSpeed)
+            {
+                Console.WriteLine(Name + " catches " + pray.Name);
+            }
+            else
+            {
+                Console.WriteLine(pray.Name + " escapes from " + Name);
+            }
+        }
+    }
+}
---
+++ Advanced_cSharp_h1/Advanced_cSharp_h1/gazzela.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_cSharp_h1
+{
+    public class Gazella : IPray
+    {
+        public int FleeSpeed { get; set; }
+        public string Name { get; set; }
+
+        public void Flee()
+        {
+            Console.WriteLine(Name + " flees away...");
+        }
+    }
+}
